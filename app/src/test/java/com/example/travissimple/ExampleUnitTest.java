38b57cb9a7d0467a29ace7f6db0618168d4fd7ca
Jose Cruz <joscruzpe@gmail.com>
2017-11-07 19:51:51 -0500

    asert

--- app/src/test/java/com/example/travissimple/ExampleUnitTest.java
+++ app/src/test/java/com/example/travissimple/ExampleUnitTest.java
@@ -10,6 +10,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(8, 1 + 2);
+        assertEquals(3, 1 + 2);
     }
 }
